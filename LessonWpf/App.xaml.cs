4753aad7ff44602a785b4b4212065cee2d1cdb31
Satytan <saty_tan@mail.ru>
2020-01-11 12:48:32 +0300

    Подключает к проекту контейнер зависимостей

--- LessonWpf/App.xaml.cs
+++ LessonWpf/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,15 @@ namespace LessonWpf
     /// </summary>
     public partial class App : Application
     {
+        void StartupDo(Object sender, StartupEventArgs e)
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddTransient<MainWindow, MainWindow>();
+
+            var provider = services.BuildServiceProvider();
+            MainWindow mainWindow = provider.GetService<MainWindow>();
+
+            mainWindow.Show();
+        }
     }
 }
